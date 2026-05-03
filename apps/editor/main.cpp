4c49f4daec87458204947a6668f42fba73fde6e8
donmaks27 <m1a9k9s6@gmail.com>
2026-05-04 00:43:32 +0200

    Split info engine-core, engine-runtime, editor and game-launcher

---
+++ apps/editor/main.cpp
@@ -0,0 +1,12 @@
+#include <engine/engine.h>
+#include <engine/utils/log.h>
+
+int main()
+{
+    if (!engine::engine::instance().start())
+    {
+        engine::log::fatal("Engine failed to start!");
+        return -1;
+    }
+    return 0;
+}
